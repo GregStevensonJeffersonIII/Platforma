bf46a0fd20d8c73b234c55bc8df0e0bd93401798
GregStevensonJeffersonIII <sandlesjames23@gmail.com>
2025-02-03 05:30:33 -0500

    more movement yay

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,45 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Vector2 movementVector;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
-   
-    public float jumpForce=100;
+    private bool isGrounded = false;
+    private bool jump = false;
+    private Rigidbody2D body;
+
+    public float gravMult = 2f;
+    public float jumpForce;
     public float speed;
+    public float maxSpeed = 7f;
     void Start()
     {
-        animator =GetComponent<Animator>();
-        spriteRenderer=GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        animator.SetFloat("speed",Mathf.Abs(movementVector.x));
-        if (movementVector.x > 0)
+        animator.SetFloat("speed", Mathf.Abs(movementVector.x));
+        if (movementVector.x > 0 && body.velocity.x<maxSpeed)
         {
             spriteRenderer.flipX = false;
-            transform.Translate(Vector2.right * speed*Time.deltaTime);
-        }
-        else if (movementVector.x < 0) {
-            spriteRenderer.flipX=true;
-            transform.Translate(Vector2.left *speed* Time.deltaTime);
-        }
-        if (movementVector.y > 1) {
-            transform.Translate(Vector2.up*movementVector.y*Time.deltaTime);
-            movementVector.y *= 0.9f;
-        }
+            //transform.Translate(Vector2.right * speed * Time.deltaTime);
+            body.AddForce(Vector2.right * speed);
 
+        }
+        else if (movementVector.x < 0 && Mathf.Abs(body.velocity.x)<maxSpeed)
+        {
+            spriteRenderer.flipX = true;
+            //transform.Translate(Vector2.left * speed * Time.deltaTime);
+            body.AddForce(Vector2.left * speed);
+        }
+        if (jump)
+        {
+            //StartCoroutine(LerpJump());
+            body.AddForce(Vector2.up * jumpForce);
+            jump = false;
+            isGrounded = false;
+        }
+        if (body.velocity.y < 0)
+        {
+            body.gravityScale = gravMult;
+        }
+        else { 
+            body.gravityScale = 0.5f;
+        }
     }
-    public void OnMove(InputValue movementValue) {
+    public void OnMove(InputValue movementValue)
+    {
         movementVector = movementValue.Get<Vector2>();
     }
 
     public void OnJump()
     {
-        movementVector.y = jumpForce;
-}
+        if (isGrounded)
+        {
+            jump = true;
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    IEnumerator LerpJump()
+    {
+        float desired =1.5f;
+        while (desired>0.1)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y+desired);
+            desired *= 0.7f;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Boundry"))
+            SceneManager.LoadScene(0);
+    }
+}
